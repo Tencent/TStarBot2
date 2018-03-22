6b0e9180ac09d97d590e092d73f9947b0ef117ba
balderli <35355974+balderli@users.noreply.github.com>
2018-03-22 14:52:28 +0800

    Add files via upload

--- tstarbot/agents/zerg_agent.py
+++ tstarbot/agents/zerg_agent.py
@@ -9,14 +9,11 @@ from copy import deepcopy
 
 from pysc2.agents import base_agent
 from tstarbot.strategy_mgr import ZergStrategyMgr
-from tstarbot.production_mgr import ZergProductionMgr
 from tstarbot.building_mgr import ZergBuildingMgr
-from tstarbot.resource_mgr import ZergResourceMgr
 from tstarbot.combat_mgr import ZergCombatMgr
 from tstarbot.scout_mgr import ZergScoutMgr
-from tstarbot.obs_mgr import ZergObsMgr
+from tstarbot.data.data_context import DataContext
 from tstarbot.act_mgr import ActMgr
-
 
 class ZergAgent(base_agent.BaseAgent):
     """A ZvZ Zerg agent for full game map."""
@@ -24,27 +21,23 @@ class ZergAgent(base_agent.BaseAgent):
     def __init__(self):
         super(ZergAgent, self).__init__()
 
-        self.obs_mgr = ZergObsMgr()
+        self.dc = DataContext()
+        self.act_mgr = ActMgr()
 
         self.strategy_mgr = ZergStrategyMgr()
-        self.production_mgr = ZergProductionMgr()
         self.building_mgr = ZergBuildingMgr()
-        self.resource_mgr = ZergResourceMgr()
         self.combat_mgr = ZergCombatMgr()
         self.scout_mgr = ZergScoutMgr()
-
-        self.act_mgr = ActMgr()
 
     def step(self, timestep):
         super(ZergAgent, self).step(timestep)
 
-        self.obs_mgr.update(timestep=timestep)
+        # update data context
+        self.dc.update(timestep)
 
-        self.strategy_mgr.update(self.obs_mgr, self.act_mgr)
-        self.production_mgr.update(self.obs_mgr, self.act_mgr)
-        self.building_mgr.update(self.obs_mgr, self.act_mgr)
-        self.resource_mgr.update(self.obs_mgr, self.act_mgr)
-        self.combat_mgr.update(self.obs_mgr, self.act_mgr)
-        self.scout_mgr.update(self.obs_mgr, self.act_mgr)
+        self.strategy_mgr.update(self.dc, self.act_mgr)
+        self.building_mgr.update(self.dc, self.act_mgr)
+        self.combat_mgr.update(self.dc, self.act_mgr)
+        self.scout_mgr.update(self.dc, self.act_mgr)
 
         return self.act_mgr.pop_actions()
