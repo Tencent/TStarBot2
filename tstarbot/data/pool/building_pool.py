18c9c4498c1fd22efc7f5441051c1854be8b6c9c
409090303@qq.com <35355974+balderli@users.noreply.github.com>
2018-03-29 17:26:13 +0800

    add EnemyPool

--- tstarbot/data/pool/building_pool.py
+++ tstarbot/data/pool/building_pool.py
@@ -1,5 +1,5 @@
 from tstarbot.data.pool.pool_base import PoolBase
-from pysc2.lib.typeenums import UNIT_TYPEID
+from tstarbot.data.pool import macro_def as tm
 
 class Building(object):
     def __init__(self, unit):
@@ -22,33 +22,6 @@ class Building(object):
 class BuildingPool(PoolBase):
     def __init__(self):
         super(PoolBase, self).__init__()
-
-        self.building_types = set([
-            # Zerg
-            UNIT_TYPEID.ZERG_HATCHERY.value,
-            UNIT_TYPEID.ZERG_SPINECRAWLER.value,
-            UNIT_TYPEID.ZERG_SPORECRAWLER.value,
-            UNIT_TYPEID.ZERG_EXTRACTOR.value,
-            UNIT_TYPEID.ZERG_SPAWNINGPOOL.value,
-            UNIT_TYPEID.ZERG_EVOLUTIONCHAMBER.value,
-            UNIT_TYPEID.ZERG_ROACHWARREN.value,
-            UNIT_TYPEID.ZERG_BANELINGNEST.value,
-            UNIT_TYPEID.ZERG_CREEPTUMOR.value,
-            UNIT_TYPEID.ZERG_LAIR.value,
-            UNIT_TYPEID.ZERG_HYDRALISKDEN.value,
-            UNIT_TYPEID.ZERG_LURKERDENMP.value,
-            UNIT_TYPEID.ZERG_SPIRE.value,
-            UNIT_TYPEID.ZERG_NYDUSNETWORK.value,
-            UNIT_TYPEID.ZERG_INFESTATIONPIT.value,
-            UNIT_TYPEID.ZERG_HIVE.value,
-            UNIT_TYPEID.ZERG_GREATERSPIRE.value,
-            UNIT_TYPEID.ZERG_ULTRALISKCAVERN.value,
-
-            # Terran
-
-            # Protoss
-
-        ])
         self._buildings = {}    # unit_tag -> Building
 
     def update(self, timestep):
@@ -79,7 +52,7 @@ class BuildingPool(PoolBase):
         for u in units:
             # print("type {}, owner {}, alliance {}".format(u.int_attr.unit_type, u.int_attr.owner, u.int_attr.alliance))
 
-            if u.int_attr.unit_type in self.building_types and u.int_attr.alliance == 1:
+            if u.int_attr.unit_type in tm.BUILDING_UNITS and u.int_attr.alliance == tm.AllianceType.SELF.value:
                 # print("building found: tag {}, type {}".format(u.int_attr.tag, u.int_attr.unit_type))
                 tag = u.int_attr.tag
                 self._buildings[tag] = Building(u)
--- tstarbot/data/pool/enemy_pool.py
+++ tstarbot/data/pool/enemy_pool.py
@@ -1,8 +1,121 @@
 from tstarbot.data.pool.pool_base import PoolBase
+from tstarbot.data.pool import macro_def as tm
+
+class EnemyGroup(object):
+    def __init__(self):
+        self.num_of_total_units = 0
+        self.num_of_worker = 0
+        self.num_of_enemy = 0
+
+        self._minerals = []
+        self._worker_units = []
+        self._enemy_units = []
+        self._central_point = {}
+
+    def add_mineral(self, mineral_unit):
+        self._minerals.append(mineral_unit)
+        self._calc_central_point()
+
+    def add_unit(self, u):
+        if u.int_attr.unit_type in tm.COMBAT_UNITS:
+            self._enemy_units.append(u)
+        elif u.int_attr.unit_type in tm.WORKER_UNITS:
+            self._worker_units.append(u)
+
+        self._calc_central_point()
+
+    def do_stat(self):
+        self.num_of_worker = len(self._worker_units)
+        self.num_of_enemy = len(self._enemy_units)
+        self.num_of_total_units = self.num_of_worker + self.num_of_enemy
+
+    def get_central_point(self):
+        return self._central_point
+
+    def calc_distance(self, u):
+        x = self._central_point['pos_x'] - u.float_attr.pos_x
+        y = self._central_point['pos_y'] - u.float_attr.pos_y
+
+        return (x*x + y*y) ** 0.5
+
+    def is_in_mineral_group(self, u):
+        MAX_MINERAL_DISTANCE = 15
+        for m in self._minerals:
+            if self._calc_distance(m, u) < MAX_MINERAL_DISTANCE:
+                return True
+
+        return False
+
+    def _calc_central_point(self):
+        all = self._minerals + self._enemy_units
+        total_x = 0.0
+        total_y = 0.0
+        for u in all:
+            total_x += u.float_attr.pos_x
+            total_y += u.float_attr.pos_y
+
+        self._central_point = {'pos_x': total_x / len(all), 'pos_y': total_y / len(all)}
+
+    def _calc_distance(self, u1, u2):
+        x = u1.float_attr.pos_x - u2.float_attr.pos_x
+        y = u1.float_attr.pos_y - u2.float_attr.pos_y
+        return (x*x + y*y) ** 0.5
 
 class EnemyPool(PoolBase):
     def __init__(self):
         super(PoolBase, self).__init__()
+        self._enemy_group = []
 
     def update(self, timestep):
-        pass
+        self._enemy_group.clear()
+
+        # find all mineral
+        units = timestep.observation['units']
+        for u in units:
+            if u.int_attr.unit_type in tm.MINIERAL_UNITS:
+                #print('find mineral ({}, {})'.format(u.float_attr.pos_x, u.float_attr.pos_y))
+                mineral_group = self._find_enemy_group_by_mineral(u)
+                if mineral_group == None:
+                    new_group = EnemyGroup()
+                    new_group.add_mineral(u)
+                    self._enemy_group.append(new_group)
+                else:
+                    mineral_group.add_mineral(u)
+
+        # add enemy units
+        for u in units:
+            if u.int_attr.alliance == tm.AllianceType.ENEMY.value:
+                #print("found enemy unit {}".format(u.int_attr.unit_type))
+                enemy_group = self._find_enemy_group_by_unit(u)
+
+                if enemy_group == None:
+                    print("ERROR: Can not find a base!!")
+                    continue
+
+                enemy_group.add_unit(u)
+
+        # do_stat
+        for g in self._enemy_group:
+            g.do_stat()
+
+    def get_enemy_group(self):
+        return self._enemy_group
+
+    def _find_enemy_group_by_mineral(self, mineral_unit):
+        for g in self._enemy_group:
+            if g.is_in_mineral_group(mineral_unit):
+                return g
+
+        return None
+
+    def _find_enemy_group_by_unit(self, army_unit):
+        distance = 100000000.0
+        group = None
+
+        for g in self._enemy_group:
+            d = g.calc_distance(army_unit)
+            if d < distance:
+                distance = d
+                group = g
+
+        return group
